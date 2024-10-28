59c8f4bfc7641e76d08a6a7e4ff4b13887f4ec20
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-10-28 14:13:26 +0100

    ajout liste-tableau cocktails & creation des components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+const cocktails = [
+  {
+    id: 1,
+    name: "Mojito",
+    image: "./assets/images/mojito.png",
+    ingredients: "Eau gazeuse, citron vert, menthe fraîche, sucre",
+    isFavorite: false,
+    addedToCart: false,
+  },
+  {
+    id: 2,
+    name: "Tequila Sunrise",
+    image: "./assets/images/tequila_sunrise.png",
+    ingredients: "Tequila, jus d'orange, sirop de grenadine",
+    isFavorite: false,
+    addedToCart: false,
+  },
+  {
+    id: 3,
+    name: "Canaille",
+    image: "./assets/images/canaille.png",
+    ingredients: "Vodka, ginger beer, citron, sirop de sureau",
+    isFavorite: false,
+    addedToCart: false,
+  },
+  {
+    id: 4,
+    name: "Daiquiri",
+    image: "./assets/images/daiquiri.png",
+    ingredients: "Rhum passion,jus de cirton vert, sirop de sucre",
+    isFavorite: false,
+    addedToCart: false,
+  },
+  {
+    id: 5,
+    name: "Sex on the beach",
+    image: "./assets/images/sex-on-the-beach.png",
+    ingredients: "Vodka, liqueur de pêche, jus d'orange, jus d'ananas",
+    isFavorite: false,
+    addedToCart: false,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Cocktails [2SLN]</h1>
+      </header>
+      <main>
+        <h2>Blabla</h2>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
